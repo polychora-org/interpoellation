ce7c0545004872811bf8c0d03ebca8225d9bd2cf
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2018-12-31 01:22:48 -0500

    WIP: makefile is circular, needs to include voices explicitly

--- Makefile
+++ Makefile
@@ -6,5 +6,9 @@ all: audio
 
 audio: $(subst raw/,,$(audio_files))
 
+speak: speak.sh
+	mkdir -p audio/raw
+	bash speak.sh mb-us1 mb-us2 mb-us3
+
 audio/%.wav: audio/raw/%.wav
 	sox $^ $@ gain -1 rate 44100
--- speak.sh
+++ speak.sh
@@ -2,7 +2,7 @@
 
 SPEED=80
 
-for v in mb-us{1,2,3}; do
+for v in $@; do
     espeak -v $v -s $SPEED -p 0 -l 1000 -w audio/raw/$v-0.wav -f phonemes.txt &
     espeak -v $v -s $SPEED -p 50 -l 1000 -w audio/raw/$v-50.wav -f phonemes.txt &
     espeak -v $v -s $SPEED -p 99 -l 1000 -w audio/raw/$v-99.wav -f phonemes.txt &
